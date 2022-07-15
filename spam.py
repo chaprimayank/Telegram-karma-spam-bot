3cc5b793643e34cf5b94910effac822b81392e91
chaprimayank <94171814+chaprimayank@users.noreply.github.com>
2022-07-15 21:17:08 +0530

    Update spam.py

--- spam.py
+++ spam.py
@@ -1,14 +1,23 @@
 #credits @themayankparmar (Telegram)
 
-from pyrogram import Client, filters
+from pyrogram import Client
 import asyncio
-app=client(session_name=Pyro String, api_id=apiidhere, api_hash=apihashhere)
- 
+from config import *
+
+API_ID = API_ID
+API_HASH = API_HASH
+SESSION = SESSION
+CHAT_ID = CHAT_ID
+MSG_ID = MSG_ID
+
+app = Client(api_id=Config.API_ID, api_hash=Config.API_HASH, session_name=Config.SESSION
+)
+
 async def main() :
     async with app:
         while true:
-            await app.send_message(message.chat.id, " pro ", reply_to_message_id=urmessageid)
+            await app.send_message(CHAT_ID, "++", reply_to_message_id=MSG_ID)
             await asyncio.sleep(5)
- 
- 
+
+
 app.run(main())
