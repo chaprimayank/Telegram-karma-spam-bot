19c7b28c58960382066628f1e16b6dd33b10917b
chaprimayank <94171814+chaprimayank@users.noreply.github.com>
2022-07-15 21:18:37 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os 
+
+class Config:
+      API_ID= int(os.environ['API_ID']
+      API_HASH =os.environ['API_HASH']
+      SESSION =os.environ['SESSION']
+      CHAT_ID =os.environ['CHAT_ID']
+      MSG_ID=os.environ['MSG_ID']
