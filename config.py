2989e012d6c66664166201f9139b1973dd727117
chaprimayank <94171814+chaprimayank@users.noreply.github.com>
2022-07-15 21:39:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
-import os 
-
+import os
 class Config:
-      API_ID= int(os.environ['API_ID']
-      API_HASH =os.environ['API_HASH']
-      SESSION =os.environ['SESSION']
-      CHAT_ID =os.environ['CHAT_ID']
-      MSG_ID=os.environ['MSG_ID']
+    API_ID = int(os.environ.get("API_ID", 0))
+    API_HASH = str(os.environ.get("API_HASH", None))
+    SESSION = str(os.environ.get("SESSION", None))
+    CHAT_ID = str(os.environ.get("CHAT_ID", None))
+    MSG_ID = int(os.environ.get("MSG_ID", None))
