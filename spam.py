e8f4c831a13cb22aedd3b13eacd786faa1dc30a4
chaprimayank <94171814+chaprimayank@users.noreply.github.com>
2022-07-15 22:16:37 +0530

    Update spam.py

--- spam.py
+++ spam.py
@@ -10,13 +10,16 @@ SESSION = Config.SESSION
 CHAT_ID = Config.CHAT_ID
 MSG_ID = Config.MSG_ID
 
+print(MSG_ID)
+print(CHAT_ID)
+
 app = Client(api_id=Config.API_ID,api_hash=Config.API_HASH,session_name=Config.SESSION
 )
 
 async def main() :
     async with app:
         while True:
-            await app.send_message(Config.CHAT_ID, "++", reply_to_message_id=Config.MSG_ID)
+            await app.send_message(Config.CHAT_ID,"++",reply_to_message_id=Config.MSG_ID)
             await asyncio.sleep(15)
 
 
