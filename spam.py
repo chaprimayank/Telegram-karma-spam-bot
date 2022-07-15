eef2fe129e77ed74da8eae521adac9753c2e0cc9
chaprimayank <94171814+chaprimayank@users.noreply.github.com>
2022-07-15 18:50:13 +0530

    Create spam.py

---
+++ spam.py
@@ -0,0 +1,14 @@
+#credits @themayankparmar (Telegram)
+
+from pyrogram import Client, filters
+import asyncio
+app=client(session_name=Pyro String, api_id=apiidhere, api_hash=apihashhere)
+ 
+async def main() :
+    async with app:
+        while true:
+            await app.send_message(message.chat.id, " pro ", reply_to_message_id=urmessageid)
+            await asyncio.sleep(15)
+ 
+ 
+app.run(main())
