7b5a64a2663844379131506d33a259913a536e0d
chaprimayank <94171814+chaprimayank@users.noreply.github.com>
2022-07-16 10:19:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,3 +5,5 @@ class Config:
     SESSION = str(os.environ.get("SESSION", None))
     CHAT_ID = int(os.environ.get("CHAT_ID", None))
     MSG_ID = int(os.environ.get("MSG_ID", None))
+    TIME = int(os.environ.get("TIME", None))
+    MESSAGE = str(os.environ.get("MESSAGE", None))
