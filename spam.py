512d0bf1a9b9fb248badaab8538cc2d2458a3a48
chaprimayank <94171814+chaprimayank@users.noreply.github.com>
2022-07-16 10:20:32 +0530

    Update spam.py

--- spam.py
+++ spam.py
@@ -1,5 +1,3 @@
-#credits @themayankparmar
-
 from pyrogram import Client
 import asyncio
 from config import *
@@ -9,9 +7,13 @@ API_HASH = Config.API_HASH
 SESSION = Config.SESSION
 CHAT_ID = Config.CHAT_ID
 MSG_ID = Config.MSG_ID
+TIME = Config.TIME
+MESSAGE = Config.MESSAGE
 
 print(MSG_ID)
 print(CHAT_ID)
+print(TIME)
+print(MESSAGE)
 
 app = Client(api_id=Config.API_ID,api_hash=Config.API_HASH,session_name=Config.SESSION
 )
@@ -19,8 +21,8 @@ app = Client(api_id=Config.API_ID,api_hash=Config.API_HASH,session_name=Config.S
 async def main() :
     async with app:
         while True:
-            await app.send_message(Config.CHAT_ID,"++",reply_to_message_id=Config.MSG_ID)
-            await asyncio.sleep(15)
+            await app.send_message(Config.CHAT_ID,"MESSAGE",reply_to_message_id=Config.MSG_ID)
+            await asyncio.sleep(Config.TIME)
 
 
 app.run(main())
